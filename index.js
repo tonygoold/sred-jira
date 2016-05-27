62bf302679a264fa910a2b9f7998fabd4794174e
Joe Drew <joe@wattpad.com>
2016-05-27 14:57:41 -0400

    Add express, render some stuff.

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 'use strict';
 
-var JiraApi = require('jira').JiraApi;
+var express = require('express');
+var hbs = require('express-hbs');
 var util = require('util');
+var JiraApi = require('jira').JiraApi;
 var config = require('../sred-jira-config.json');
 
 var jira = new JiraApi('https', config.host, config.port, config.user, config.password, '2');
-jira.findIssue('WEB-5257', function(error, issue) {
-      console.log(util.inspect(issue, true, null));
+var app = express();
+
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res) {
+  var query = req.query.query || 'project = web';
+  jira.searchJira(query, { fields: ['*all'], expand:['changelog'] }, function(error, body, resp) {
+    if (error) {
+      res.render('index', {
+        errors: error.errorMessages,
+        query: query
+      });
+    } else {
+      res.render('index', {
+        query: query,
+        issues: body.issues
+      });
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
 });
