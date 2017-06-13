a0ebbf64eabb0a33e4e78ee16a3396cbe7d6655d
Nino D'Aversa <nino@ndaversa.com>
2017-06-13 11:24:33 -0400

    Marcosify all the things

--- jiraquery.js
+++ jiraquery.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var JiraApi = require('jira').JiraApi;
-var util = require('util');
-var moment = require('moment');
-var _ = require('lodash');
+var JiraApi = require( 'jira' ).JiraApi;
+var util = require( 'util' );
+var moment = require( 'moment' );
+var _ = require( 'lodash' );
 
 try {
-  var config = require('../sred-jira-config.json');
-} catch(e) {
+  var config = require( '../sred-jira-config.json' );
+}
+catch ( e ) {
   var config = {
     host: process.env.JIRA_HOST,
     port: process.env.JIRA_PORT,
@@ -16,220 +17,223 @@ try {
   };
 }
 
-var jira = new JiraApi('https', config.host, config.port, config.user, config.password, '2');
+var jira = new JiraApi( 'https', config.host, config.port, config.user, config.password, '2' );
 
 var issueTypes = {
-	1: 'Bug',
-	3: 'Task',
-	15: 'Experiment',
-	19: 'Story',
-	20: 'Exploratory'
+  1: 'Bug',
+  3: 'Task',
+  15: 'Experiment',
+  19: 'Story',
+  20: 'Exploratory'
 };
 
 var statuses = {
-	11431: 'Triage',
-	10018: 'Backlog',
-	10019: 'Selected for Development',
-	3: 'In Progress',
-	10005: 'In Review',
-	10331: 'Merged',
-	10017: 'For Approval'
+  11431: 'Triage',
+  10018: 'Backlog',
+  10019: 'Selected for Development',
+  3: 'In Progress',
+  10005: 'In Review',
+  10331: 'Merged',
+  10017: 'For Approval'
 }
 
 var activeStatuses = [
-	3, 10004, 10005, 10026
+  3, 10004, 10005, 10026
 ];
 
 function Calendar() {
-	this.people = {};
+  this.people = {};
 }
 
-Calendar.prototype.addPersonDay = function(person, date) {
-	var days = this.people[person];
-	if (!days) {
-		days = [];
-		this.people[person] = days;
-	}
-	var inArray = days.some(function(day) {
-		return day.isSame(date);
-	});
-	if (!inArray) {
-		days.push(date);
-	}
+Calendar.prototype.addPersonDay = function( person, date ) {
+  var days = this.people[ person ];
+  if ( !days ) {
+    days = [];
+    this.people[ person ] = days;
+  }
+  var inArray = days.some( function( day ) {
+    return day.isSame( date );
+  } );
+  if ( !inArray ) {
+    days.push( date );
+  }
 };
 
-Calendar.prototype.addPersonDays = function(person, days) {
-	days.forEach(function(day) {
-		this.addPersonDay(person, day);
-	}, this);
+Calendar.prototype.addPersonDays = function( person, days ) {
+  days.forEach( function( day ) {
+    this.addPersonDay( person, day );
+  }, this );
 };
 
 Calendar.prototype.getPeople = function() {
-	var people = [];
-	for (var person in this.people) {
-		people.push(person);
-	}
-	return people;
+  var people = [];
+  for ( var person in this.people ) {
+    people.push( person );
+  }
+  return people;
 };
 
-Calendar.prototype.getWorkingDays = function(person) {
-	var days = this.people[person];
-	if (!days) {
-		return [];
-	}
-	return days.filter(function(day) {
-		var weekday = day.day();
-		return weekday != 0 && weekday != 6;
-	});
+Calendar.prototype.getWorkingDays = function( person ) {
+  var days = this.people[ person ];
+  if ( !days ) {
+    return [];
+  }
+  return days.filter( function( day ) {
+    var weekday = day.day();
+    return weekday != 0 && weekday != 6;
+  } );
 };
 
-Calendar.prototype.getWorkingHours = function(person) {
-	return this.getWorkingDays(person).length * 8;
+Calendar.prototype.getWorkingHours = function( person ) {
+  return this.getWorkingDays( person ).length * 8;
 };
 
-Calendar.prototype.addCalendar = function(calendar) {
-	calendar.getPeople().forEach(function(person) {
-		this.addPersonDays(person, calendar.getWorkingDays(person));
-	}, this);
+Calendar.prototype.addCalendar = function( calendar ) {
+  calendar.getPeople().forEach( function( person ) {
+    this.addPersonDays( person, calendar.getWorkingDays( person ) );
+  }, this );
 };
 
-function Transition(user, fromStatus, toStatus, date) {
-	this.user = user;
-	this.fromStatus = fromStatus;
-	this.toStatus = toStatus;
-	this.date = date;
+function Transition( user, fromStatus, toStatus, date ) {
+  this.user = user;
+  this.fromStatus = fromStatus;
+  this.toStatus = toStatus;
+  this.date = date;
 }
 
-function Issue(issueJson) {
-	this.ticket = issueJson.key;
-	this.project = issueJson.fields.project.key;
-	this.assignee = _.result(issueJson, 'fields.assignee.name');
-	var transitions = [];
-	issueJson.changelog.histories.forEach(function(history) {
-		var user = history.author.name;
-		var date = moment(history.created).startOf('day');
-		history.items.forEach(function(item) {
-			if (item.fieldtype == 'jira' && item.field == 'status') {
-				transitions.push(new Transition(user, parseInt(item.from, 10), parseInt(item.to, 10), date));
-			}
-		});
-	});
-	this.transitions = transitions;
+function Issue( issueJson ) {
+  this.ticket = issueJson.key;
+  this.project = issueJson.fields.project.key;
+  this.assignee = _.result( issueJson, 'fields.assignee.name' );
+  var transitions = [];
+  issueJson.changelog.histories.forEach( function( history ) {
+    var user = history.author.name;
+    var date = moment( history.created ).startOf( 'day' );
+    history.items.forEach( function( item ) {
+      if ( item.fieldtype == 'jira' && item.field == 'status' ) {
+        transitions.push( new Transition( user, parseInt( item.from, 10 ), parseInt( item.to, 10 ), date ) );
+      }
+    } );
+  } );
+  this.transitions = transitions;
 }
 
-Issue.prototype.getActiveDays = function () {
-	var lastStart = null;
-	var days = [];
-	this.transitions.forEach(function(transition) {
-		var fromActive = activeStatuses.indexOf(transition.fromStatus) >= 0;
-		var toActive = activeStatuses.indexOf(transition.toStatus) >= 0;
-		if (!fromActive && toActive) {
-			lastStart = transition.date;
-		} else if (fromActive && !toActive) {
-			var day = moment(lastStart);
-			while (day.isSameOrBefore(transition.date)) {
-				days.push(day);
-				day = moment(day);
-				day.add(1, 'd');
-			}
-			lastStart = null;
-		}
-	}, this);
-	return days;
+Issue.prototype.getActiveDays = function() {
+  var lastStart = null;
+  var days = [];
+  this.transitions.forEach( function( transition ) {
+    var fromActive = activeStatuses.indexOf( transition.fromStatus ) >= 0;
+    var toActive = activeStatuses.indexOf( transition.toStatus ) >= 0;
+    if ( !fromActive && toActive ) {
+      lastStart = transition.date;
+    }
+    else if ( fromActive && !toActive ) {
+      var day = moment( lastStart );
+      while ( day.isSameOrBefore( transition.date ) ) {
+        days.push( day );
+        day = moment( day );
+        day.add( 1, 'd' );
+      }
+      lastStart = null;
+    }
+  }, this );
+  return days;
 };
 
-function Project(name) {
-	this.name = name;
-	this.issues = [];
+function Project( name ) {
+  this.name = name;
+  this.issues = [];
 }
 
-Project.prototype.addIssue = function(issue) {
-	var index = this.issues.findIndex(function(otherIssue) {
-		return otherIssue.ticket == issue.ticket;
-	});
-	if (index >= 0) {
-		this.issues[index] = issue;
-	} else {
-		this.issues.push(issue);
-	}
+Project.prototype.addIssue = function( issue ) {
+  var index = this.issues.findIndex( function( otherIssue ) {
+    return otherIssue.ticket == issue.ticket;
+  } );
+  if ( index >= 0 ) {
+    this.issues[ index ] = issue;
+  }
+  else {
+    this.issues.push( issue );
+  }
 };
 
-Project.prototype.removeIssue = function(ticketNumber) {
-	var index = this.issues.findIndex(function(otherIssue) {
-		return otherIssue.ticket == issue.ticket;
-	});
-	if (index >= 0) {
-		this.issues.splice(index, 1);
-	}
+Project.prototype.removeIssue = function( ticketNumber ) {
+  var index = this.issues.findIndex( function( otherIssue ) {
+    return otherIssue.ticket == issue.ticket;
+  } );
+  if ( index >= 0 ) {
+    this.issues.splice( index, 1 );
+  }
 };
 
 Project.prototype.getCalendar = function() {
-	var calendar = new Calendar();
-	this.issues.forEach(function(issue) {
-		calendar.addPersonDays(issue.assignee, issue.getActiveDays());
-	});
-	return calendar;
+  var calendar = new Calendar();
+  this.issues.forEach( function( issue ) {
+    calendar.addPersonDays( issue.assignee, issue.getActiveDays() );
+  } );
+  return calendar;
 };
 
 function JiraQuery() {
-	this.projects = {};
+  this.projects = {};
 };
 
-JiraQuery.prototype.getProject = function(name) {
-	var project = this.projects[name];
-	if (!project) {
-		project = new Project(name);
-		this.projects[name] = project;
-	}
-	return project;
+JiraQuery.prototype.getProject = function( name ) {
+  var project = this.projects[ name ];
+  if ( !project ) {
+    project = new Project( name );
+    this.projects[ name ] = project;
+  }
+  return project;
 };
 
 JiraQuery.prototype.getProjects = function() {
-	var projects = [];
-	for (var name in this.projects) {
-		projects.push(this.projects[name]);
-	}
-	return projects;
+  var projects = [];
+  for ( var name in this.projects ) {
+    projects.push( this.projects[ name ] );
+  }
+  return projects;
 };
 
-JiraQuery.prototype.addTicket = function(ticketNumber) {
-	var self = this;
-	return new Promise(function (resolve, reject) {
-		jira.findIssue(ticketNumber + '?expand=changelog', function(error, issueJson) {
-			if (error) {
-				reject(error);
-			}
+JiraQuery.prototype.addTicket = function( ticketNumber ) {
+  var self = this;
+  return new Promise( function( resolve, reject ) {
+    jira.findIssue( ticketNumber + '?expand=changelog', function( error, issueJson ) {
+      if ( error ) {
+        reject( error );
+      }
 
-			try {
-				var issue = new Issue(issueJson);
-				var project = self.getProject(issue.project);
-				project.addIssue(issue);
-				resolve(issue);
-			} catch (ex) {
-				console.log("Caught an exception: " + ex.stack);
-				reject(ex);
-			}
-		});
-	});
+      try {
+        var issue = new Issue( issueJson );
+        var project = self.getProject( issue.project );
+        project.addIssue( issue );
+        resolve( issue );
+      }
+      catch ( ex ) {
+        console.log( "Caught an exception: " + ex.stack );
+        reject( ex );
+      }
+    } );
+  } );
 };
 
-JiraQuery.prototype.addTickets = function(ticketNumbers) {
-	return Promise.all(ticketNumbers.map(function(ticketNumber) {
-		return this.addTicket(ticketNumber);
-	}, this));
+JiraQuery.prototype.addTickets = function( ticketNumbers ) {
+  return Promise.all( ticketNumbers.map( function( ticketNumber ) {
+    return this.addTicket( ticketNumber );
+  }, this ) );
 };
 
-JiraQuery.prototype.removeTicket = function(ticketNumber) {
-	this.getProjects().forEach(function(project) {
-		project.removeTicket(ticketNumber);
-	});
+JiraQuery.prototype.removeTicket = function( ticketNumber ) {
+  this.getProjects().forEach( function( project ) {
+    project.removeTicket( ticketNumber );
+  } );
 }
 
 JiraQuery.prototype.getCalendar = function() {
-	return this.getProjects().reduce(function(previous, project) {
-		previous.addCalendar(project.getCalendar());
-		return previous;
-	}, new Calendar());
+  return this.getProjects().reduce( function( previous, project ) {
+    previous.addCalendar( project.getCalendar() );
+    return previous;
+  }, new Calendar() );
 }
 
 /* Usage:
